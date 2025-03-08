b3ed46f213de13b56a126c70b1cb1d36a7df51f4
jogg15 <jogger.cztechnic@gmail.com>
2025-03-08 10:35:59 +0100

    first commit

---
+++ src/main/java/me/jogg/imperium/ImperiumClient.java
@@ -0,0 +1,11 @@
+package me.jogg.imperium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImperiumClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/me/jogg/imperium/ImperiumDataGenerator.java
@@ -0,0 +1,11 @@
+package me.jogg.imperium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ImperiumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
